77a3d1062acff8d875dda6e11664b8862966f781
Mark Lewis <mlewis@trinity.edu>
2021-03-06 17:08:11 -0600

    Added a page for a sorted stats table Web 1.0 style.

--- play-scalajs-slinky-master/server/app/controllers/Application.scala
+++ play-scalajs-slinky-master/server/app/controllers/Application.scala
@@ -1,15 +1,22 @@
-package edu.trinity.videoquizreact.controllers
+package controllers
 
 import javax.inject._
 
 import edu.trinity.videoquizreact.shared.SharedMessages
 import play.api.mvc._
+import models.DougStatData
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
     Ok(views.html.index(SharedMessages.itWorks))
+  }
+
+  def table(col: Int) = Action {
+    val sortedData = if (col == 0) DougStatData.data.sortBy(_.name) 
+      else DougStatData.data.sortBy(_.stats(col - 1))
+    Ok(views.html.table(sortedData.toSeq))
   }
 
 }
